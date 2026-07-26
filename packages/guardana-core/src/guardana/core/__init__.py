967951a9e9919f5a03ed254e2e5ca5efe4727361
Konrad Karauda <konrad@karauda.com>
2026-07-26 11:24:09 +0200

    chore(release): v0.2.0

--- packages/guardana-core/src/guardana/core/__init__.py
+++ packages/guardana-core/src/guardana/core/__init__.py
@@ -12,7 +12,7 @@ from guardana.core.surface import Surface
 from guardana.core.target import Capability, Target, TargetKind
 from guardana.core.taxonomy import TaxonomyRef
 
-__version__ = "0.1.3"
+__version__ = "0.2.0"
 
 __all__ = [
     "Capability",
